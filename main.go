5f5e7bd4f4c798debef017ad649cef07f99b19c5
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-09-07 14:01:21 +0700

    feat: function count L and K

--- main.go
+++ main.go
@@ -2,31 +2,30 @@ package main
 
 import "fmt"
 func main() {
-	luas := luas(5,10)
-	fmt.Println(luas)
-
-	keliling := keliling(5,10)
-	fmt.Println(keliling)
-
+	// luas(5,10)
+	// keliling(5,10)
 	K,L := LnK(5,10)
-	fmt.Println(K,L)
+	fmt.Printf("hasil keliling persegi: %d\nhasil luas persegi: %d\n", K, L)
 }
 
 // rumus luas : panjang x lebar
-func luas(panjang uint8, lebar uint8) (luas uint8){
-	luas = panjang * lebar
+func luas(panjang uint8, lebar uint8) uint8 {
+	luas := panjang * lebar
 	return luas
 }
 
 //rumus keliling : 2 x (panjang x lebar)
-func keliling(panjang uint8, lebar uint8) (keliling uint8){
-	keliling = 2 * (panjang * lebar)
-	return keliling	
+func keliling(panjang uint8, lebar uint8) uint8 {
+	keliling := 2 * (panjang * lebar)
+	return keliling
 }
 
 // keliling dan lebar
-func LnK(panjang uint8, lebar uint8)(K uint8, L uint8){
-	K = keliling(5,10)
-	L = luas(5,10)
-	return K, L
+func LnK(panjang uint8, lebar uint8) (hasilKeliling uint8, hasilLuas uint8){
+	hasilKeliling = keliling(panjang, lebar)
+	// keliling := 2 * (panjang * lebar)
+	hasilLuas = luas(panjang, lebar)
+	// luas := panjang * lebar	
+	// fmt.Printf("hasil keliling persegi: %d\nhasil luas persegi: %d\n", K, L)
+	return hasilKeliling, hasilLuas
 }
