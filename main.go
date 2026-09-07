854d57765a85e3800b9b64fedfcf843d203a8a07
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-09-07 15:12:11 +0700

    feat: func window

--- main.go
+++ main.go
@@ -1,31 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
 func main() {
 	// luas(5,10)
 	// keliling(5,10)
-	K,L := LnK(5,10)
-	fmt.Printf("hasil keliling persegi: %d\nhasil luas persegi: %d\n", K, L)
+	// K,L := LnK(5,10)
+	// fmt.Printf("hasil keliling persegi: %d\nhasil luas persegi: %d\n", K, L)
+	err := buildWindow(5)
+	if err != nil{
+		fmt.Printf("error : %s", err)
+		return
+	}
+}
+
+func buildWindow(window uint8) error{
+	if window < 5 {
+		return fmt.Errorf("window must be at least 5")
+	}
+
+	for vertical := uint8(0); vertical < window; vertical++ {
+		for horizontal := uint8(0); horizontal < window; horizontal++ {
+			if vertical == 0 || vertical == window-1 || horizontal == 0 || horizontal == window-1 {
+				fmt.Printf("* ")
+			} else {
+				fmt.Printf("  ")
+			}
+		}
+		fmt.Println()
+	}
+
+	return nil
 }
 
 // rumus luas : panjang x lebar
-func luas(panjang uint8, lebar uint8) uint8 {
-	luas := panjang * lebar
-	return luas
-}
+// func luas(panjang uint8, lebar uint8) uint8 {
+// 	luas := panjang * lebar
+// 	return luas
+// }
 
 //rumus keliling : 2 x (panjang x lebar)
-func keliling(panjang uint8, lebar uint8) uint8 {
-	keliling := 2 * (panjang * lebar)
-	return keliling
-}
+// func keliling(panjang uint8, lebar uint8) uint8 {
+// 	keliling := 2 * (panjang * lebar)
+// 	return keliling
+// }
 
 // keliling dan lebar
-func LnK(panjang uint8, lebar uint8) (hasilKeliling uint8, hasilLuas uint8){
-	hasilKeliling = keliling(panjang, lebar)
-	// keliling := 2 * (panjang * lebar)
-	hasilLuas = luas(panjang, lebar)
-	// luas := panjang * lebar	
-	// fmt.Printf("hasil keliling persegi: %d\nhasil luas persegi: %d\n", K, L)
-	return hasilKeliling, hasilLuas
-}
+// func LnK(panjang uint8, lebar uint8) (hasilKeliling uint8, hasilLuas uint8){
+// 	hasilKeliling = keliling(panjang, lebar)
+// 	// keliling := 2 * (panjang * lebar)
+// 	hasilLuas = luas(panjang, lebar)
+// 	// luas := panjang * lebar	
+// 	// fmt.Printf("hasil keliling persegi: %d\nhasil luas persegi: %d\n", K, L)
+// 	return hasilKeliling, hasilLuas
+// }
